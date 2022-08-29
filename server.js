5be6497128954f8eaf707194f23aea06ab47bc52
Amanda Perry <challenge641@gmail.com>
2022-08-29 15:23:05 -0500

    setting up to use cookies

--- server.js
+++ server.js
@@ -2,29 +2,38 @@ const path = require('path');
 const express = require('express');
 // set up Handlebars.js as app's template engine of choice
 const exphbs = require('express-handlebars');
+// importing the connection to sequelize from config/connection.js
+const sequelize = require('./config/connection');
+
+// sets up an Express session and connects the session to our Sequelize db
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret', // replace with actual secret to store in .env
+    cookie: {}, // tells our session to use cookies
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// importing the connection to sequelize from config/connection.js
-const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-// express.static() method is a built-in Express.js middleware function that 
-// can take all of the contents of a folder and serve them as static assets. 
-// Useful for front-end specific files like images, style sheets, and js files.
 app.use(require('./controllers/'));
 
-// turn on connection to db and the server
-// The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables. 
-// NOTE* If it doesn't find a table, it'll create it.
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
